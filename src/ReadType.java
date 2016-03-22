d34af303ebe98f59d8b63baf2df1e92f0c622442
Matan <pitzuach.team@gmail.com>
2016-03-22 23:42:50 +0200

    fix

--- src/ReadType.java
+++ src/ReadType.java
@@ -2,5 +2,16 @@
  * Created by Matan on 3/22/16.
  */
 public class ReadType {
-    int
+    int numLines;
+    String parsedString;
+
+    public ReadType() {
+        numLines = 0;
+        parsedString = null;
+    }
+
+    public ReadType(int numLines, String parsedString) {
+        this.numLines = numLines;
+        this.parsedString = parsedString;
+    }
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Matan on 3/22/16.
@@ -8,14 +9,14 @@ import java.io.IOException;
 public class Reader {
 
 
-    public String readFromTxt(String filename, String parsedString) throws IOException {
+    public ReadType readFromTxt(String filename, String parsedString) throws IOException {
         BufferedReader br = null;
         String currentLine;
         br = new BufferedReader(new FileReader("/Users/Matan/IdeaProjects/Anti-Bullying/KS.txt"));
         while ((currentLine = br.readLine()) != null) {
             parsedString.concat(currentLine);
         }
-        return parsedString;
+        ReadType readType = new ReadType((int) (long) br.lines().count(), parsedString);
+        return readType;
     }
-
 }
